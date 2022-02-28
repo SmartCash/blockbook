1a1aae0214319f5600710093a5065502ac4fca79
TheSolarminer <solarminer@smartcash.cc>
2022-02-28 01:56:38 -0600

    Updates

--- bchain/coins/smartcash/smartcashparser.go
+++ bchain/coins/smartcash/smartcashparser.go
@@ -3,19 +3,22 @@ package smartcash
 import (
 	"github.com/martinboehm/btcd/wire"
 	"github.com/martinboehm/btcutil/chaincfg"
-	"github.com/trezor/blockbook/bchain/coins/btc"
+        "github.com/SmartCash/blockbook/bchain"
+        "github.com/SmartCash/blockbook/bchain/coins/smartcash"
 )
+//       "github.com/trezor/blockbook/bchain"
+//       "github.com/trezor/blockbook/bchain/coins/btc"
 
 // network constants
 const (
 	MainnetMagic wire.BitcoinNet = 0x5ca1ab1e
-	TestnetMagic wire.BitcoinNet = 0xcffcbeea
+//	TestnetMagic wire.BitcoinNet = 0xcffcbeea
 )
 
 // parser parameters
 var (
 	MainNetParams chaincfg.Params
-	TestNetParams chaincfg.Params
+//	TestNetParams chaincfg.Params
 )
 
 func init() {
@@ -25,11 +28,11 @@ func init() {
 	MainNetParams.ScriptHashAddrID = []byte{18}
 	MainNetParams.Bech32HRPSegwit = "sc"
 
-	TestNetParams = chaincfg.TestNet3Params
-	TestNetParams.Net = TestnetMagic
-	TestNetParams.PubKeyHashAddrID = []byte{65}
-	TestNetParams.ScriptHashAddrID = []byte{21}
-	TestNetParams.Bech32HRPSegwit = "sct"
+//	TestNetParams = chaincfg.TestNet3Params
+//	TestNetParams.Net = TestnetMagic
+//	TestNetParams.PubKeyHashAddrID = []byte{65}
+//	TestNetParams.ScriptHashAddrID = []byte{21}
+//	TestNetParams.Bech32HRPSegwit = "sct"
 }
 
 // SmartCashParser handle
@@ -45,7 +48,7 @@ func NewSmartCashParser(params *chaincfg.Params, c *btc.Configuration) *SmartCas
 // GetChainParams contains network parameters for the main SmartCash network
 // and the SmartCash Testnet network
 func GetChainParams(chain string) *chaincfg.Params {
-	if !chaincfg.IsRegistered(&MainNetParams) {
+/*	if !chaincfg.IsRegistered(&MainNetParams) {
 		err := chaincfg.Register(&MainNetParams)
 		if err == nil {
 			err = chaincfg.Register(&TestNetParams)
@@ -58,6 +61,6 @@ func GetChainParams(chain string) *chaincfg.Params {
 	case "test":
 		return &TestNetParams
 	default:
-		return &MainNetParams
-	}
+*/		return &MainNetParams
+//	}
 }
